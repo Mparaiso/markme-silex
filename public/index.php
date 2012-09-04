bd6a937d9e4128c75d3137998378b501a6e85f04
mparaiso <aikah@free.fr>
2012-09-05 00:45:53 +0200

    index modified

--- public/index.php
+++ public/index.php
@@ -21,16 +21,18 @@ $app->get('/', function() {
     return 'Hello! To test this Silex app, put your name at the end of the URL in the address bar above! For example: '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'John';
 });
 
-# Hello {name} example
-$app->get('/{name}', function($name) use($app) {
-    return 'Hello, '.$app->escape($name).'!';
-});
 # info
 if($app['debug']===true):
 	$app->get('/info',function(Application $app){
 		return phpinfo();
 	});
 endif;
+
+# Hello {name} example
+$app->get('/{name}', function($name) use($app) {
+    return 'Hello, '.$app->escape($name).'!';
+});
+
 # Run the app
 $app->run();
 ?>
