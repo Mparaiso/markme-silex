5706ffa9305897e06b50a387207443b10c6301a8
mparaiso <aikah@free.fr>
2012-09-06 03:17:17 +0200

    modif

--- public/index.php
+++ public/index.php
@@ -13,12 +13,26 @@ $loader = require_once(dirname(__DIR__).'/vendor/autoload.php');
 
 # Create new app
 $app = new Silex\Application();
+$app['config.mongodb_server']=getenv("mongodb_server");
+$app['config.mongodb_database']=getenv("mongodb_database");
+
 $app['autoloader'] = $app->share(function(Application $app)use($loader){
 	return $loader;
 });
 $app['autoloader']->add("App",dirname(__DIR__));
+
+### SERVICES 
+
 $app->register(new MonologServiceProvider(),
 	array("monolog.logfile"=>dirname(__DIR__)."/log/application.log")
+);
+$app['monolog.handler'] = $app->share(function(Application $app){
+		return new Monolog\Handler\MongoDBHandler(
+			$app['config.mongodb_server'],
+			$app['config.mongodb_database'],
+			"log"
+			);
+	}
 );
 $app->mount('/',new IndexController());
 # Enable debugging
