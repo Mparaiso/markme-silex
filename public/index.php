5dc3dd6c92612ac4b40d6347d5fba5a9dff1855a
mparaiso <aikah@free.fr>
2012-09-06 14:43:11 +0200

    commit

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-#putenv('MONGODB_SERVER=mongodb://camus:defender@alex.mongohq.com:10079/mparaiso');
-#putenv('MONGODB_DATABASE=mpraiso');
+if($_SERVER['HTTP_HOST']!="mparaiso.local"):
+	putenv('MONGODB_SERVER=mongodb://camus:defender@alex.mongohq.com:10079/mparaiso');
+	putenv('MONGODB_DATABASE=mpraiso');
+endif;
 define('ROOT',dirname(__DIR__));
 $app = require(dirname(__DIR__)."/App/bootstrap.php");
