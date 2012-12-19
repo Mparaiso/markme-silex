d8152e77ae7f2df4c641b371592e31fc920b8900
mparaiso <aikah@free.fr>
2012-12-19 19:42:33 +0100

    tests/bootstrap created

--- tests/App/AppTest.php
+++ tests/App/AppTest.php
@@ -16,11 +16,30 @@ class AppTest extends WebTestCase{
         return $app;
     }
 
+    /**
+     * Affiche la page de garde du site internet
+     */
     function testIndex(){
         $client = $this->createClient();
         $crawler = $client->request("GET","/");
 
         $this->assertTrue($client->getResponse()->isOk());
+        $this->assertCount(1,$crawler->filter("h1.title:contains('Hello Silex !')"));
+        $this->assertCount(1,$crawler->filter("body"));
+
+        $crawler = $client->request("GET","/Marc");
+        $this->assertCount(1,$crawler->filter("h1.title:contains('Hello Marc !')"));
+
+        $crawler = $client->request("GET","/Marc Prades");
+        $this->assertCount(1,$crawler->filter("h1.title:contains('Hello Marc Prades !')"));
+
+    }
+
+    /**
+     * crée un utilisateur via une requète json
+     */
+    function testJsonRegister_POST(){
+        $this->assertTrue(true);
     }
 
     function testTest(){
