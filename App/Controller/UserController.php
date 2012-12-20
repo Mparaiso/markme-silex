cd10b47a0b73cfb279591049f97469bbd661b702
mparaiso <aikah@free.fr>
2012-12-20 16:33:52 +0100

    .travis.yml updated

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use Silex\Application;
 use Doctrine\DBAL\DBALException;
+
 /**
  * FR : gère les utilisateurs de l'application
  *
  * @author M.Paraiso
  */
 class UserController {
-    
+
     /**
      * 
      * @param \Silex\Application $app
@@ -49,85 +50,84 @@ class UserController {
      */
     function login(Application $app) {
         $username = $app['request']->get("username");
-        $password =$app["request"]->get("password") ? md5($username+$app['request']->get("password")+$app["salt"]) : null ;
-        if($username AND $password):
+        $password = $app["request"]->get("password") ? md5($username + $app['request']->get("password") + $app["salt"]) : null;
+        if ($username AND $password):
             try {
-                $user = $app["db"]->fetchAssoc("SELECT id, username, email FROM users WHERE username = :username AND password = :password",array(
-                    "username"=>$username,
-                    "password"=>$password,
-                ));     
-                if($user):
+                $user = $app["db"]->fetchAssoc("SELECT id, username, email FROM users WHERE username = :username AND password = :password", array(
+                    "username" => $username,
+                    "password" => $password,
+                        ));
+                if ($user):
                     $this->_setLoggedUserSession($app, $user);
-                    $app["db"]->udpate(array("last_login"=>time()),array("id"=>$user["id"]));
-                    return $app->json(array_merge($user,array("status"=>"ok")),200,array("HTTP_Cache-Control"=>'max-age=0, must-revalidate, no-cache, no-store',"HTTP_Content-type"=>'application/javascript'));
+                    $app["db"]->udpate(array("last_login" => time()), array("id" => $user["id"]));
+                    return $app->json(array_merge($user, array("status" => "ok")), 200, array("HTTP_Cache-Control" => 'max-age=0, must-revalidate, no-cache, no-store', "HTTP_Content-type" => 'application/javascript'));
                 else:
-                    return $app->json(array("status"=>"error","message"=>"User not found"),200);
+                    return $app->json(array("status" => "error", "message" => "User not found"), 200);
                 endif;
             } catch (DBALException $exc) {
                 $app['monolog']->addError($exc->getTraceAsString());
                 $app["session"]->invalidate();
-                return  $app->json(array("status"=>"error","message"=>"Database Error"),200);
+                return $app->json(array("status" => "error", "message" => "Database Error"), 200);
             }
         else:
-            return $app->json(array("status"=>"error","message"=>"Invalid parameters"),200);
+            return $app->json(array("status" => "error", "message" => "Invalid parameters"), 200);
         endif;
     }
-    
-    
-    function logout(Application $app){
-        if($app["session"]->get("user")&&$app["session"]->get("user_id")):
-            $response = $app->json(array("status"=>"ok","message"=>"user logged out"),200);
+
+    function logout(Application $app) {
+        if ($app["session"]->get("user") && $app["session"]->get("user_id")):
+            $response = $app->json(array("status" => "ok", "message" => "user logged out"), 200);
         else:
-            $response = $app->json(array("status"=>"error","message"=>"no user found"),200);
+            $response = $app->json(array("status" => "error", "message" => "no user found"), 200);
         endif;
-       $app["session"]->invalidate();
-       return $response;
+        $app["session"]->invalidate();
+        return $response;
     }
-    
-    function getCurrent(Application $app){
+
+    function getCurrent(Application $app) {
         $user = $app["session"]->get("user");
-        if(isset($user)):
-            return $app->json(array_merge($user,array("status"=>"ok")),200);
+        if (isset($user)):
+            return $app->json(array_merge($user, array("status" => "ok")), 200);
         else:
             $app["session"]->invalidate();
-            return $app->json(array("status"=>"error","message"=>"User not found"),200);
+            return $app->json(array("status" => "error", "message" => "User not found"), 200);
         endif;
     }
-    
-    function updateUser(Application $app){
-        
+
+    function updateUser(Application $app) {
+
         $username = $app["request"]->get("username");
         $email = $app["request"]->get("email");
-        $password = $app["request"]->get("password")?md5($username+$app["request"]->get("password")+$app["salt"]):null;
-        if(isset($username)&&isset($email)):
+        $password = $app["request"]->get("password") ? md5($username + $app["request"]->get("password") + $app["salt"]) : null;
+        if (isset($username) && isset($email)):
             try {
                 $user_id = $app["session"]->get("user_id");
-                $affectedRows = $app["db"]->update("users",array("username"=>$username,"password"=>$password,"email"=>$email),array("id"=>$user_id));
-                if($affectedRows>0){
-                    $user = $app["db"]->fetchAssoc("SELECT id,username,email from users WHERE id = ?",array($user_id));
-                    if($user):
+                $affectedRows = $app["db"]->update("users", array("username" => $username, "password" => $password, "email" => $email), array("id" => $user_id));
+                if ($affectedRows > 0) {
+                    $user = $app["db"]->fetchAssoc("SELECT id,username,email from users WHERE id = ?", array($user_id));
+                    if ($user):
                         $this->_setLoggedUserSession($app, $user);
-                        return $app->json(array("username"=>$user["username"],"email"=>$user["email"],"status"=>"ok"),200);
+                        return $app->json(array("username" => $user["username"], "email" => $user["email"], "status" => "ok"), 200);
                     endif;
                 }
             } catch (DBALException $exc) {
                 $app["logger"]->addError($exc->getTraceAsString());
             }
         else:
-            return $app->json(array("status"=>"error","message"=>"Unable to update user"),200);
+            return $app->json(array("status" => "error", "message" => "Unable to update user"), 200);
         endif;
-
     }
-    /** 
+
+    /**
      * si un utilisateur valide existe , parametrer la session
      * @param type $app
      * @param type $user
      */
-    protected function _setLoggedUserSession($app,$user){
+    protected function _setLoggedUserSession($app, $user) {
         $app['session']->set("user_id", $user["id"]);
-        $app["session"]->set("user", array("id" => $user["id"],"username" => $user["username"], "email" => $user["email"]));
+        $app["session"]->set("user", array("id" => $user["id"], "username" => $user["username"], "email" => $user["email"]));
     }
-    
+
 }
 
 ?>
