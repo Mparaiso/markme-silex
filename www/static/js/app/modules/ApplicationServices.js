d71aeb3735b16bdf82beadf3fe7ad227b1ff772e
mparaiso <aikah@free.fr>
2013-01-05 18:41:42 +0100

    application.js modified

--- www/static/js/app/modules/ApplicationServices.js
+++ www/static/js/app/modules/ApplicationServices.js
@@ -1,61 +1,70 @@
 //service concernant la gestion d'un utilisateur
 
 var ApplicationServices = angular.module("ApplicationServices", []);
-
+ApplicationServices.factory("Url", function() {
+    return{
+        getBase: function() {
+            return ($("meta[name=base_url]").attr("content")) || "";
+        }
+    };
+});
 /** manage user API calls **/
-ApplicationServices.factory("UserService", ['$http', '$window',
-    function($http, $window) {
+ApplicationServices.factory("UserService", ['$http', '$window', 'Url',
+    function($http, $window, Url) {
         var config = {cache: true};
+        var baseUrl = Url.getBase();
         return {
             getCurrentUser: function(success, error) {
-                $http.get("/json/user").success(success).error(error || function() {
+                $http.get(baseUrl + "/json/user").success(success).error(error || function() {
                 });
             },
             isLogged: function() {
             },
             register: function(user, success, error) {
-                $http.post("/json/register", user).success(success).error(error);
+                $http.post(baseUrl + "/json/register", user).success(success).error(error);
             },
             login: function(user, success, error) {
-                $http.post("/json/login", user).success(success).error(error);
+                $http.post(baseUrl + "/json/login", user).success(success).error(error);
             },
             logout: function(success, error) {
-                $http.post("/json/logout").success(success).error(error);
+                $http.post(baseUrl + "/json/logout").success(success).error(error);
             }
         };
     }
 ]);
 
 /** manage bookmark API calls **/
-ApplicationServices.factory("BookmarkService", ['$http', function($http) {
+ApplicationServices.factory("BookmarkService", ['$http', "Url", function($http, Url) {
         var config = {cache: true};
+        var baseUrl = Url.getBase();
         return {
             get: function(success, error) {
-                $http.get("/json/bookmark", config).success(success).error(error);
+                $http.get(baseUrl + "/json/bookmark", config).success(success).error(error);
             },
             put: function(bookmark, success, error) {
-                $http.put("/json/bookmark", bookmark).success(success).error(error);
+                $http.put(baseUrl + "/json/bookmark", bookmark).success(success).error(error);
             },
             post: function(bookmark, success, error) {
-                $http.post("/json/bookmark", bookmark).success(success).error(error);
+                $http.post(baseUrl + "/json/bookmark", bookmark).success(success).error(error);
             },
             "delete": function(id, success, error) {
-                $http['delete']("/json/bookmark/" + id).success(success).error(error);
+                $http['delete'](baseUrl + "/json/bookmark/" + id).success(success).error(error);
 
             },
             "getByTag": function(tagName, success, error) {
-                $http.get("/json/bookmark/tag/" + tagName, config).success(success).error(error);
+                $http.get(baseUrl + "/json/bookmark/tag/" + tagName, config).success(success).error(error);
             },
             bookmark: {}
         };
     }]);
 
 /** manage tag API calls **/
-ApplicationServices.factory("TagService", ["$http", function($http) {
+ApplicationServices.factory("TagService", ["$http", "Url", function($http, Url) {
         var config = {cache: true};
+        var baseUrl = Url.getBase();
         return {
             get: function(success, error) {
-                $http.get("/json/tag", config).success(success).error(error);
+                $http.get(baseUrl + "/json/tag", config).success(success).error(error);
             }
         };
     }]);
