80047e6bbda401a24620d8413cf1d4e99c6f0828
mparaiso <aikah@free.fr>
2014-08-07 17:10:53 +0200

    markme v1 refactoring Thu Aug  7 15:10:52 GMT 2014

--- web/index.php
+++ web/index.php
@@ -1,3 +1,18 @@
 <?php
-$app = require("../App/application.php");
+
+
+use MarkMe\App;
+
+$autoload = require(__DIR__.'/../vendor/autoload.php');
+$autoload->add('MarkMe',__DIR__."/../");
+
+# router for php builtin server router
+
+$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+$debug = getenv('PHP_ENV') == "production" ? FALSE : TRUE;
+$app = new App(array('debug' => $debug));
 $app->run();
